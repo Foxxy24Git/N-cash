eac41f65a59326af4e662e9bb5d43ce7886e57a3
Foxxy24Git <kurnia24fajri@gmail.com>
2026-04-24 12:41:23 +0700

    fix: exclude all /api/* from middleware to allow route handlers to return 401

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,6 @@ export const { auth: middleware } = NextAuth(authConfig)
 
 export const config = {
   matcher: [
-    '/((?!api/auth|_next/static|_next/image|favicon.ico|login).*)',
+    '/((?!api/|_next/static|_next/image|favicon.ico|login).*)',
   ],
 }
